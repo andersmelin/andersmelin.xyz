47e8e78515172a3a03bde87a41e81ff2f772788f
Anders Melin <am222vp@student.lnu.se>
2016-04-03 22:07:40 +0545

    Minimal Express app

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var app = express();
+
+app.get("/", (req, res) => {
+    res.status("200").send("<a href='http://cloudnotes.andersmelin.xyz'>CloudNotes</a>");
+});
+
+// Handle 404
+app.use(function(req, res) {
+    res.status(404).send('404: Page not Found');
+});
+
+// Handle 500
+app.use(function(error, req, res, next) {
+    res.status(500).send('500: Internal Server Error');
+});
